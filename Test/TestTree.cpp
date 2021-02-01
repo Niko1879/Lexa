0e77324c6c4d36e5148a474cc2ba59fb95092d9b
Niko <nik_77@live.co.uk>
2021-02-01 19:53:00 +0000

    updated unit test to use 1 move and 1 copy

--- Test/TestTree.cpp
+++ Test/TestTree.cpp
@@ -6,9 +6,10 @@ namespace InterpreterTest
 {
 	TEST(TestTree, TestConstruct)
 	{
+		Tok t{ Type::Number, "1" };
 		EXPECT_NO_THROW(
 			Lexa::Tree<Tok> left(Tok{ Type::Number, "1" });
-			Lexa::Tree<Tok> right(Tok{ Type::Number, "1" });
+			Lexa::Tree<Tok> right(t);
 			Lexa::Tree<Tok> root(Tok{Type::Operation, "+"}, std::move(left), std::move(right));
 		);
 	}
