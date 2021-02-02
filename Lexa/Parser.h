7a8bc81a62bf6fb6f306044e811373693efba43e
Niko <nik_77@live.co.uk>
2021-02-02 18:27:11 +0000

    moved enums and constants to their own file

--- Lexa/Parser.h
+++ Lexa/Parser.h
@@ -10,15 +10,6 @@ namespace Lexa
 	{
 		using ParseTree = Tree<Token>;
 
-		const std::unordered_map<Operation, int> OperatorPrecedence
-		{
-			{Operation::Plus, 0},
-			{Operation::Minus, 0},
-			{Operation::Times, 1},
-			{Operation::Divide, 1},
-			{Operation::Raise, 2},
-		};
-
 		ParseTree Parse(const std::vector<Token>& tokens);
 	}
 }
---
+++ Lexa/Types.h
@@ -0,0 +1,123 @@
+#pragma once
+#include <unordered_map>
+
+
+namespace Lexa
+{
+	namespace Interpreter
+	{
+		enum class TokenType
+		{
+			Number,
+			Operation,
+			LeftBracket,
+			RightBracket,
+			Variable,
+			Function,
+		};
+
+
+		enum class Operation
+		{
+			Plus,
+			Minus,
+			Times,
+			Divide,
+			Raise,
+		};
+
+
+		enum class Function
+		{
+			Exp,
+			Log,
+			Sin,
+			Cos,
+			Tan,
+			Sec,
+			Cosec,
+			Cot,
+			Sinh,
+			Cosh,
+			Tanh,
+			Sech,
+			Cosech,
+			Coth,
+			Arcsin,
+			Arccos,
+			Arctan,
+			Arcsec,
+			Arccosec,
+			Arccot,
+			Arsinh,
+			Arcosh,
+			Artanh,
+			Arsech,
+			Arcosech,
+			Arcoth,
+		};
+
+
+		const std::unordered_map<std::string, Operation> StrToOperation
+		{
+			{"+", Operation::Plus},
+			{"-", Operation::Minus},
+			{"*", Operation::Times},
+			{"/", Operation::Divide},
+			{"^", Operation::Raise},
+		};
+
+
+		const std::unordered_map<std::string, Function> StrToFunction
+		{
+			{"exp", Function::Exp},
+			{"log", Function::Log},
+			{"ln", Function::Log},
+			{"sin", Function::Sin},
+			{"cos", Function::Cos},
+			{"tan", Function::Tan},
+			{"sec", Function::Sec},
+			{"cosec", Function::Cosec},
+			{"cot", Function::Cot},
+			{"sinh", Function::Sinh},
+			{"cosh", Function::Cosh},
+			{"tanh", Function::Tanh},
+			{"sech", Function::Sech},
+			{"cosech", Function::Cosech},
+			{"coth", Function::Coth},
+			{"arcsin", Function::Arcsin},
+			{"arccos", Function::Arccos},
+			{"arctan", Function::Arctan},
+			{"arcsec", Function::Arcsec},
+			{"arccosec", Function::Arccosec},
+			{"arccot", Function::Arccot},
+			{"arsinh", Function::Arsinh},
+			{"arcosh", Function::Arcosh},
+			{"artanh", Function::Artanh},
+			{"arsech", Function::Arsech},
+			{"arcosech", Function::Arcosech},
+			{"artanh", Function::Artanh},
+		};
+
+
+		const std::unordered_map<TokenType, std::string> StrToTokenType
+		{
+			{TokenType::Number, "Number"},
+			{TokenType::Operation, "Operation"},
+			{TokenType::LeftBracket, "LeftBracket"},
+			{TokenType::RightBracket, "RightBracket"},
+			{TokenType::Variable, "Variable"},
+			{TokenType::Function, "Function"},
+		};
+
+
+		const std::unordered_map<Operation, int> OperatorPrecedence
+		{
+			{Operation::Plus, 0},
+			{Operation::Minus, 0},
+			{Operation::Times, 1},
+			{Operation::Divide, 1},
+			{Operation::Raise, 2},
+		};
+	}
+}
