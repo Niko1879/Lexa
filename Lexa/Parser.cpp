0227172f254d6beb16361833e4e4d34cecc6b060
Niko <nik_77@live.co.uk>
2021-02-02 17:37:53 +0000

    passing test for parenthesised expressions

--- Lexa/Parser.cpp
+++ Lexa/Parser.cpp
@@ -31,7 +31,10 @@ namespace Lexa
 
 				case TokenType::Operation:
 
-					while (stack.size() > 0 && OperatorPrecedence.at(Operations.at(stack.back().value)) >= OperatorPrecedence.at(Operations.at(t.value)))
+					while (stack.size() > 0 && 
+						   stack.back().type != TokenType::LeftBracket &&
+						   OperatorPrecedence.at(Operations.at(stack.back().value)) >= 
+						   OperatorPrecedence.at(Operations.at(t.value)))
 					{
 						_Pop(partialTrees, stack);
 					}
@@ -39,6 +42,17 @@ namespace Lexa
 					stack.push_back(t);
 					break;
 
+				case TokenType::LeftBracket:
+					stack.push_back(t);
+					break;
+
+				case TokenType::RightBracket:
+					while (stack.size() > 0 && stack.back().type != TokenType::LeftBracket)
+					{
+						_Pop(partialTrees, stack);
+					}
+					if (stack.size() > 0) stack.pop_back();
+					break;
 
 				default:
 					throw std::invalid_argument("Could not parse expression");
--- Test/TestParser.cpp
+++ Test/TestParser.cpp
@@ -32,4 +32,38 @@ namespace InterpreterTest
 		EXPECT_NO_THROW(Lexa::Interpreter::Parse(tokens));
 		EXPECT_EQ(root, Lexa::Interpreter::Parse(tokens));
 	}
+
+
+	TEST(TestParser, TestParseParenthesised)
+	{
+		std::vector<Tok> tokens = {
+		   Tok{Type::LeftBracket, "("},
+		   Tok{Type::Number, "4"},
+		   Tok{Type::Operation, "-"},
+		   Tok{Type::Number, "3"},
+		   Tok{Type::RightBracket, ")"},
+		   Tok{Type::Operation, "^"},
+		   Tok{Type::Number, "3"},
+		   Tok{Type::Operation, "/"},
+		   Tok{Type::LeftBracket, "("},
+		   Tok{Type::Number, "1"},
+		   Tok{Type::Operation, "+"},
+		   Tok{Type::Number, "3"},
+		   Tok{Type::RightBracket, ")"},
+		};
+
+		Lexa::Tree<Tok> l1(Tok{ Type::Number, "4" });
+		Lexa::Tree<Tok> r1(Tok{ Type::Number, "3" });
+		Lexa::Tree<Tok> t1(Tok{ Type::Operation, "-" }, std::move(l1), std::move(r1));
+		Lexa::Tree<Tok> t2(Tok{ Type::Number, "3" });
+		Lexa::Tree<Tok> p1(Tok{ Type::Operation, "^" }, std::move(t1), std::move(t2));
+		
+		Lexa::Tree<Tok> l2(Tok{ Type::Number, "1" });
+		Lexa::Tree<Tok> r2(Tok{ Type::Number, "3" });
+		Lexa::Tree<Tok> p2(Tok{ Type::Operation, "+" }, std::move(l2), std::move(r2));
+		Lexa::Tree<Tok> root(Tok{ Type::Operation, "/" }, std::move(p1), std::move(p2));
+
+		EXPECT_NO_THROW(Lexa::Interpreter::Parse(tokens));
+		EXPECT_EQ(root, Lexa::Interpreter::Parse(tokens));
+	}
 }
